7884b100319fc349ea864ad790ba0d960d9b3247
DhulipudiM <monica.dhulipudi@gmail.com>
2020-11-08 18:54:50 -0500

    footer with links rough draft

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
-
+import {Footer} from './footer/Footer';
 function App() {
   return (
     <div className="App">
     <>
       <h1 style={{color: 'green'}}>Parking Finder Coming Soon...</h1>
     </>
+
+    <Footer />
     </div>
   );
 }
